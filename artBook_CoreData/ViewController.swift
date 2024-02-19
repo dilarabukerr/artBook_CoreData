3798b31fd40dd63123debe38312ecdbb7c3d5535
Dilara Büker <72442245+dilarabukerr@users.noreply.github.com>
2024-02-19 17:55:10 +0300

    Initial Commit

---
+++ artBook_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  artBook_CoreData
+//
+//  Created by Dilara Büker on 19.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
